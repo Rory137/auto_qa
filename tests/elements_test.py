d479b7a5cbe947d94e737ca892510b5bd4c4b53a
rory <nextemus@gmail.com>
2024-01-20 16:11:15 +0300

    update elements tests added test checkboxes

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,7 @@
 import time
 
 from pages.base_page import BasePage
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 # def test(driver):
@@ -25,3 +25,15 @@ class TestElements:
             assert current_address == output_current_addres, "the current address does not mach"
             assert permanent_address == output_per_adrres, "the permanent address does not mach"
 
+    class TestCheckBox:
+
+        def test_check_box(self,driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            time.sleep(5)
+            check_box_page.click_random_checkbox()
+            time.sleep(10)
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, "checkboxes have not been selected"
