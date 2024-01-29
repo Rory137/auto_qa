8ecb76eb9449f7aa26d8d2750b9add9e1d6a5736
rory <nextemus@gmail.com>
2024-01-29 21:30:13 +0300

    update elements tests added tests: -test_web_table_add_person -test_web_table_search_person

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -1,9 +1,7 @@
 from selenium.webdriver.common.by import By
-
 class TextBoxPageLocators:
 
     # form fields
-
     FULL_NAME = (By.CSS_SELECTOR, "input[id='userName']")
     EMAIL = (By.CSS_SELECTOR, "input[id='userEmail']")
     CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea[id='currentAddress']")
@@ -11,12 +9,10 @@ class TextBoxPageLocators:
     SUBMIT = (By.CSS_SELECTOR, "button[id='submit']")
 
     #created from
-
     CREATED_FULL_NAME = (By.CSS_SELECTOR, "#output #name")
     CREATED_EMAI = (By.CSS_SELECTOR, "#output #email")
     CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, "#output #currentAddress")
     CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "#output #permanentAddress")
-
 
 
 class CheckBoxPageLocators:
@@ -34,5 +30,19 @@ class RadioButtonPageLocators:
     OUTPUT_RESULT = (By.CSS_SELECTOR, "p span[class='text-success']")
 
 
+class WebTablePageLocators:
+    # add person form
+    ADD_BUTTON = (By.CSS_SELECTOR, 'button[id="addNewRecordButton"]')
+    FIRSTNAME_INPUT = (By.CSS_SELECTOR, 'input[id="firstName"]')
+    LASTNAME_INPUT = (By.CSS_SELECTOR, 'input[id="lastName"]')
+    EMAIL_INPUT = (By.CSS_SELECTOR, 'input[id="userEmail"]')
+    AGE_INPUT = (By.CSS_SELECTOR, 'input[id="age"]')
+    SALARY_INPUT = (By.CSS_SELECTOR, 'input[id="salary"]')
+    DEPARTMENT_INPUT = (By.CSS_SELECTOR, 'input[id="department"]')
+    SUBMIT = (By.CSS_SELECTOR, 'button[id="submit"]')
 
-
+    #tables
+    FULL_PEOPLE_LIST = (By.CSS_SELECTOR, "div[class='rt-tr-group']")
+    SEARCH_INPUT = (By.CSS_SELECTOR, "input[id='searchBox']")
+    DELETE_BUTTON = (By.CSS_SELECTOR, "span[title='Delete']")
+    ROW_PARENT = ".//ancestor::div[@class='rt-tr-group']"
